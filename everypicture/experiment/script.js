2cfe57f6f372eb37e24eaeae457ef42cae8ec6b9
Giulia_Zappia <giuliazappia@ucsb.edu>
2026-02-16 16:45:50 -0800

    experiment

--- everypicture/experiment/script.js
+++ everypicture/experiment/script.js
@@ -2,7 +2,6 @@
     'use strict';
     console.log('reading js');
 
-    const container = document.querySelector('#container');
     const hotspots = document.querySelectorAll('#container div');
     const theImg = document.querySelector('div img');
 
@@ -11,17 +10,22 @@
         eachSpot.addEventListener('mouseout', function(){
             theImg.className='start';
         });
+
+        eachSpot.addEventListener('click', function(event){
+            if(event.target.id === 'center'){
+                theImg.src = 'images/open_S.jpg';
+                //this is very very close to what I want BUT I dont want the image to be zoomed in
+                //I ma experiment with innerHTML more
+            }
+        });
     });
 
     function zoomPhoto(event){
         const thisCorner = event.target.id;
         console.log(thisCorner);
         switch(thisCorner){
-            // case 'topleft': theImg.className='topleft'; break;
-            // case 'topright': theImg.className='topright'; break;
-            // case 'bottomleft': theImg.className='bottomleft'; break;
-            // case 'bottomright': theImg.className='bottomright'; break;
             case 'center': theImg.className='center'; break;
         }
     }
+
 })();
