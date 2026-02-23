ffa3a637142920f1e45f59a36158074a620c4052
Giulia_Zappia <giuliazappia@ucsb.edu>
2026-02-23 13:49:08 -0800

    EP_progress

--- everypicture/cv1_final/script.js
+++ everypicture/cv1_final/script.js
@@ -2,21 +2,22 @@
     'use strict';
     console.log('reading js');
 
-    const hotspots = document.querySelectorAll('#center, #left, #right');
+    const hotspots = document.querySelectorAll('#scene2, #scene3, #scene4');
     const theImg = document.querySelector('#scene1 .backgroundImg');
+    const open = document.querySelector('#scene4');
 
     console.log(hotspots);
 
     hotspots.forEach(function(eachSpot){
         eachSpot.addEventListener('mouseover', zoomPhoto);
         eachSpot.addEventListener('mouseout', function(){
-            theImg.className='start';
+            theImg.className='backgroundImg';
         });
 
         eachSpot.addEventListener('click', function(event){
-            if(event.target.id === 'center'){
-                theImg.src = 'images/open_S.jpg';
-                //this is very very close to what I want BUT I dont want the image to be zoomed in
+            if(event.target.id === 'scene4'){
+                open.remove.className='hideme';
+                open.style.zIndex='3';
             }
         });
     });
@@ -25,11 +26,11 @@
         const thisCorner = event.target.id;
         console.log(thisCorner);
         switch(thisCorner){
-            case 'center': theImg.className='center'; break;
-            case 'left': theImg.className='left'; break;
-            case 'right': theImg.className='right'; break;
+            case 'scene4': theImg.className='scene4'; break;
+            case 'scene2': theImg.className='scene2'; break;
+            case 'scene3': theImg.className='scene3'; break;
         }
     }
 
-    //plan: incorporate z-index and .hideme into the zoom for scene 1
+    //plan: incorporate z-index and .hideme into the zoom
 })();
