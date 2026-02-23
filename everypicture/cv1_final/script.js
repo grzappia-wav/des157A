e622a6ce3f42deaa2451293d8cbbef7ef442e54c
Giulia_Zappia <giuliazappia@ucsb.edu>
2026-02-22 22:45:42 -0800

    EveryPictureProgress

---
+++ everypicture/cv1_final/script.js
@@ -0,0 +1,34 @@
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    const hotspots = document.querySelectorAll('#center, #left, #right');
+    const theImg = document.querySelector('#scene2 .backgroundImg');
+
+    console.log(hotspots);
+
+    hotspots.forEach(function(eachSpot){
+        eachSpot.addEventListener('mouseover', zoomPhoto);
+        eachSpot.addEventListener('mouseout', function(){
+            theImg.className='start';
+        });
+
+        eachSpot.addEventListener('click', function(event){
+            if(event.target.id === 'center'){
+                theImg.src = 'images/open_S.jpg';
+                //this is very very close to what I want BUT I dont want the image to be zoomed in
+                //I ma experiment with innerHTML more
+            }
+        });
+    });
+
+    function zoomPhoto(event){
+        const thisCorner = event.target.id;
+        console.log(thisCorner);
+        switch(thisCorner){
+            case 'center': theImg.className='center'; break;
+            case 'left': theImg.className='left'; break;
+            case 'right': theImg.className='right'; break;
+        }
+    }
+})();
