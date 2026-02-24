6b75a3f8ad9b12cbebdfae9b1fb1643a903d6b1c
Giulia_Zappia <giuliazappia@ucsb.edu>
2026-02-23 20:07:24 -0800

    EP_Progress

--- everypicture/cv1_final/script.js
+++ everypicture/cv1_final/script.js
@@ -36,16 +36,39 @@
 
 
     const scene2 = document.querySelector('#scene2');
+    const scene3 = document.querySelector('#scene3');
     const scene4 = document.querySelector('#scene4');
-    const scene4Img=document.querySelector('#scene4 img');
+    const scene4Img = document.querySelector('#scene4 img');
     const scene5 = document.querySelector('#scene5');
     
     scene2.addEventListener('mouseover', function(){
         // console.log('over');
         const scene1Img = document.querySelector('#scene1 img');
         scene1Img.className='scene2';
-
         scene2.style.opacity='1';
+    });
+
+    scene2.addEventListener('mouseout', function(){
+        // console.log('over');
+        const scene1Img = document.querySelector('#scene1 img');
+        scene1Img.classList.remove='scene2';
+        scene1Img.className='scene1';
+        scene2.style.opacity='0';
+    });
+
+    scene3.addEventListener('mouseover', function(){
+        // console.log('over');
+        const scene1Img = document.querySelector('#scene1 img');
+        scene1Img.className='scene3';
+        scene3.style.opacity='1';
+    });
+
+    scene3.addEventListener('mouseout', function(){
+        // console.log('over');
+        const scene1Img = document.querySelector('#scene1 img');
+        scene1Img.classList.remove='scene3';
+        scene1Img.className='scene1';
+        scene2.style.opacity='0';
     });
 
     scene4.addEventListener('click', function(){
@@ -54,7 +77,7 @@
 
     scene4Img.addEventListener( 'transitionend', function(){
         scene5.removeAttribute('class');
-        scene5.style.zIndex='2'
+        scene5.style.zIndex='7'
     }, {once: true} );
 
 })();
