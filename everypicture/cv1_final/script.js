72f3be098d5746bda191e0159e6a33cc5ab0bc1a
Giulia_Zappia <giuliazappia@ucsb.edu>
2026-02-23 16:00:26 -0800

    Progress!

--- everypicture/cv1_final/script.js
+++ everypicture/cv1_final/script.js
@@ -2,35 +2,59 @@
     'use strict';
     console.log('reading js');
 
-    const hotspots = document.querySelectorAll('#scene2, #scene3, #scene4');
-    const theImg = document.querySelector('#scene1 .backgroundImg');
-    const open = document.querySelector('#scene4');
+    // const hotspots = document.querySelectorAll('#scene2, #scene3, #scene4');
+    // const theImg = document.querySelector('#scene1 .backgroundImg');
+    // const open = document.querySelector('#scene4');
 
-    console.log(hotspots);
+    // console.log(hotspots);
 
-    hotspots.forEach(function(eachSpot){
-        eachSpot.addEventListener('mouseover', zoomPhoto);
-        eachSpot.addEventListener('mouseout', function(){
-            theImg.className='backgroundImg';
-        });
+    // hotspots.forEach(function(eachSpot){
+    //     eachSpot.addEventListener('mouseover', zoomPhoto);
+    //     eachSpot.addEventListener('mouseout', function(){
+    //         theImg.className='backgroundImg';
+    //     });
 
-        eachSpot.addEventListener('click', function(event){
-            if(event.target.id === 'scene4'){
-                open.remove.className='hideme';
-                open.style.zIndex='3';
-            }
-        });
-    });
+    //     eachSpot.addEventListener('click', function(event){
+    //         if(event.target.id === 'scene4'){
+    //             open.remove.className='hideme';
+    //             open.style.zIndex='3';
+    //         }
+    //     });
+    // });
 
-    function zoomPhoto(event){
-        const thisCorner = event.target.id;
-        console.log(thisCorner);
-        switch(thisCorner){
-            case 'scene4': theImg.className='scene4'; break;
-            case 'scene2': theImg.className='scene2'; break;
-            case 'scene3': theImg.className='scene3'; break;
-        }
-    }
+    // function zoomPhoto(event){
+    //     const thisCorner = event.target.id;
+    //     console.log(thisCorner);
+    //     switch(thisCorner){
+    //         case 'scene4': theImg.className='scene4'; break;
+    //         case 'scene2': theImg.className='scene2'; break;
+    //         case 'scene3': theImg.className='scene3'; break;
+    //     }
+    // }
 
     //plan: incorporate z-index and .hideme into the zoom
+
+
+    const scene2 = document.querySelector('#scene2');
+    const scene4 = document.querySelector('#scene4');
+    const scene4Img=document.querySelector('#scene4 img');
+    const scene5 = document.querySelector('#scene5');
+    
+    scene2.addEventListener('mouseover', function(){
+        // console.log('over');
+        const scene1Img = document.querySelector('#scene1 img');
+        scene1Img.className='scene2';
+
+        scene2.style.opacity='1';
+    });
+
+    scene4.addEventListener('click', function(){
+        scene4Img.style.opacity='1';
+    });
+
+    scene4Img.addEventListener( 'transitionend', function(){
+        scene5.removeAttribute('class');
+        scene5.style.zIndex='2'
+    }, {once: true} );
+
 })();
