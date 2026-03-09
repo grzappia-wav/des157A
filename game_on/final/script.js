0166780f360239eca1cde950e1aaeb4ed72c6aa8
Giulia_Zappia <giuliazappia@ucsb.edu>
2026-03-09 15:26:07 -0700

    fixes

---
+++ game_on/final/script.js
@@ -0,0 +1,123 @@
+(function(){
+    'use strict'
+    console.log('reading JS');
+
+    //overlay JS
+    document.querySelector('.open').addEventListener('click', function(){
+        document.querySelector('#overlay').className='showing';
+    });
+
+    document.querySelector('.close').addEventListener('click', function(){
+        document.querySelector('#overlay').className='hidden';
+    });
+
+    document.addEventListener('keydown', function(event){
+        if(event.key == "Escape"){
+            document.querySelector('#overlay').className='hidden';
+        }
+    });
+
+    //Game JS
+    const startGame = document.querySelector('#startgame');
+    const gameControl = document.querySelector('#gamecontrol');
+    // const beginBtn = document.querySelector('#startgame');
+    const game = document.querySelector('#game');
+    const score = document.querySelector('#score');
+    const actionArea = document.querySelector('#actions');
+    const gameData = {
+        dice: ['die1.png', 'die2.png', 'die3.png', 
+            'die4.png', 'die5.png', 'die6.png'],
+        players: ['player 1', 'player 2'],
+        score: [0, 0],
+        roll1: 0,
+        roll2: 0,
+        rollSum: 0,
+        index: 0,
+        gameEnd: 29
+    };
+
+    startGame.addEventListener('click', function(){
+        gameData.index = Math.round(Math.random()); //randomly rounds either to 1 or 0, thus selecting player 1 or 2
+        console.log(gameData.index);
+
+        // beginBtn.style.opacity = 0;
+        // gameControl.innerHTML = '<h2>The Auction has Begun!</h2>';
+        gameControl.innerHTML = '<button id ="quit">Give In?</button>';
+
+        document.querySelector('#quit').addEventListener('click', function(){
+            location.reload();
+        });
+
+        // console.log('set the turn');
+        setUpTurn();
+    }); //END start game event listener
+
+    function setUpTurn() {
+        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        actionArea.innerHTML = '<button id = "roll">Roll the Dice</button>';
+        document.querySelector('#roll').addEventListener('click', function(){
+            // console.log('Roll the Dice!')
+            throwDice()
+        });
+    } //END set up turn
+
+    function throwDice(){
+        actionArea.innerHTML='';
+        gameData.roll1 = Math.floor(Math.random()*6) + 1; //math.floor rounds down the random number (+1 is there to fix the scale from 0--5 to 1--6 cause JS starts at 0)
+        gameData.roll2 = Math.floor(Math.random()*6) + 1;
+
+        game.innerHTML = `<p>Roll the dice for ${gameData.players[gameData.index]}</p>`;
+        game.innerHTML +=  `<p><img src="images/${gameData.dice[gameData.roll1-1]}"><img src="images/${gameData.dice[gameData.roll2-1]}"></p>`;
+        gameData.rollSum =  gameData.roll1 + gameData.roll2;
+
+        if (gameData.rollSum === 2){
+            game.innerHTML += '<p>The Kracken has stolen the coins!</p>';
+            gameData.score[gameData.index]
+            //this function should set the rolling player's score to 0 but it dosen't
+
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1); //this line switches the player
+
+            setTimeout(setUpTurn, 2000);
+            // console.log('snake eyes!');
+        }
+        
+        else if (gameData.roll1 === 1 || gameData.roll2 === 1){
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            game.innerHTML += `<p>Luck is thin! Switch to ${gameData.players[gameData.index]}</p>`;
+            setTimeout(setUpTurn, 2000);
+            // console.log('one of the two dice rolled a 1');
+        }
+        
+        else {
+            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
+            actionArea.innerHTML = '<button id="rollagain">Roll</button> <button id="pass">Pass</button>';
+
+            document.querySelector('#rollagain').addEventListener('click', function(){
+                throwDice();
+            });
+
+            document.querySelector('#pass').addEventListener('click', function(){
+                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+                setUpTurn();
+            });
+            // console.log('neither die was a 1, game contiunes ...');
+        }
+
+        showCurrentScore();
+        checkWinningCondition();
+    }; //END function throw dice
+
+    function checkWinningCondition() {
+        if (gameData.score[gameData.index] > gameData.gameEnd){
+            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
+            actionArea.innerHTML = '';
+            document.querySelector('#quit').innerHTML = 'Begin a New Bid';
+        } else {
+           showCurrentScore();
+        }
+    }
+
+    function showCurrentScore(){
+            score.innerHTML = `<p>The score is currently: <strong>${gameData.players[0]}</strong>: <strong>${gameData.score[0]}</strong> and <strong>${gameData.players[1]}</strong>: <strong>${gameData.score[1]}</strong></p>`;
+        }
+})();
